3c7c4896a473294db5b956c373d46429d0bbe283
Imani Chandler <imani.j.chandler@gmail.com>
2022-01-26 23:26:19 -0800

    sixth commit

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,7 +12,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
             <div className='col-md-5 m-1'>
                 <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody><CardTitle>{campsite.name}</CardTitle>
+                    <CardBody>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
@@ -42,9 +43,21 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         if (props.campsite) {
             return (
                 <div className="container">
+                    <div className="container">
+                        <div className='row'>
+                            <div className='col'>
+                                <Breadcrumb>
+                                <BreadcrumbItem><Link to="/home">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                                </Breadcrumb>
+                                <h2>{props.campsite.name}</h2>
+                                <hr />
+                            </div>
+                        </div>
+                </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.campsite.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
