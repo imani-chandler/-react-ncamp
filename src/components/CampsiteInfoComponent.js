27e3beaa70470785533ff77d0b48eaf37cead496
Imani Chandler <imani.j.chandler@gmail.com>
2022-01-26 18:48:40 -0800

    fifth commit

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import { CAMPSITES } from '../shared/campsites';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
 
-    renderCampsite(campsite) {
+
+
+    function RenderCampsite({campsite}) {
         
         return (
             <div className='col-md-5 m-1'>
@@ -21,7 +21,7 @@ class CampsiteInfo extends Component {
     }
 
 
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments) {
             return (
                 <div className='col-md-5 m-1'>
@@ -38,18 +38,20 @@ class CampsiteInfo extends Component {
         );
     }
 
-    render() {
-        if (this.props.campsite) {
-            return (<div className='row'>
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>);
-        } else {
-            return (<div></div>);
+    function CampsiteInfo(props) {
+        if (props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                    </div>
+                </div>
+            );
         }
-    };
+        return <div />;
+    }
 
 
-}
 
 export default CampsiteInfo;
